581483a3c8653cf1f5809ba3027276bb966bc899
truthlighting <truthlighting@gmail.com>
2016-09-14 17:30:55 -0700

    Early Changes

--- main.js
+++ main.js
@@ -15,6 +15,21 @@ if (!Memory.uCreepID) {
 }
 
 module.exports.loop = function () {
+    var creep = Game.creeps['H-1'];
+
+    if(creep.carry.energy < creep.carryCapacity) {
+        var sources = creep.room.find(FIND_SOURCES);
+        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0]);
+        }
+    }
+    else {
+        if( creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
+            creep.moveTo(Game.spawns['Spawn1']);
+        }
+    }
+}
+
 
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
