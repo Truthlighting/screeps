4d3351211a850f0e209579a3a1b974bd7882fd84
truthlighting <truthlighting@gmail.com>
2016-09-14 22:38:50 -0700

    Added containers to harvesters

--- role.hauler.js
+++ role.hauler.js
@@ -1,4 +1,12 @@
 /**
  * Created by truthlighting on 9/14/2016.
  */
+var roleHauler = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        if(creep.memory.building && creep.carry.energy == 0) {
+            creep.memory.building = false;
+            creep.say('harvesting');
 module.exports = roleHauler;
