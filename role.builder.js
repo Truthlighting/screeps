c225daa1437675bc0ac6bdfd6dfb8cda6ea2f5b4
truthlighting <truthlighting@gmail.com>
2016-09-15 18:00:23 -0700

    minimizing builder idling

--- role.builder.js
+++ role.builder.js
@@ -10,7 +10,7 @@ var roleBuilder = {
             creep.memory.building = false;
             creep.say('harvesting');
         }
-        if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+        if (!creep.memory.building && creep.carry.energy == creep.carryCapacity || ((creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE)) && creep.carry.energy > 0)) {
             creep.memory.building = true;
             creep.say('building');
         }
@@ -43,8 +43,12 @@ var roleBuilder = {
 
         else {
             var target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-            if(creep.harvest(target) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(target);
+            if (target) {
+                if(creep.harvest(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            } else {
+                //do something else
             }
         }
     }
