a22842572cfc9692af5c2b7c81c79d61037e8443
truthlighting <truthlighting@gmail.com>
2016-09-19 19:55:18 -0700

    temp remove harvester to container

--- role.harvester.js
+++ role.harvester.js
@@ -32,7 +32,7 @@ var roleHarvester = {
             var energyStorageStructures = creep.room.find(FIND_STRUCTURES, {
                         filter: (structure) => {
                             return (structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_CONTAINER ||
+                //            structure.structureType == STRUCTURE_CONTAINER ||
                             structure.structureType == STRUCTURE_SPAWN ||
                             structure.structureType == STRUCTURE_TOWER) &&
                             (structure.energy < structure.energyCapacity || _.sum(structure.store) < structure.storeCapacity);
