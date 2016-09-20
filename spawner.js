0d5b3b118628e889e96c96c6e58cd1357cc58388
truthlighting <truthlighting@gmail.com>
2016-09-20 16:42:38 -0700

    Fixing miner

--- spawner.js
+++ spawner.js
@@ -122,7 +122,7 @@ module.exports =
         for (var index in bodyArray)
         {
             var body = bodyArray[index];
-            if (spawnCost(body) < spawnPoint.room.energyAvailable)
+            if (this.spawnCost(body) < spawnPoint.room.energyAvailable)
                 return body;
         }
 
