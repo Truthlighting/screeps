4ac80cfde177bc83f50df027b6075b40a3ee8611
truthlighting <truthlighting@gmail.com>
2016-09-19 18:49:10 -0700

    debugging hauler

--- role.hauler.js
+++ role.hauler.js
@@ -12,7 +12,7 @@ var roleHauler = {
                                             structure.store[RESOURCE_ENERGY] > 0
 
         })
-        console.log(notEmptyContainers);
+        console.log(notEmptyContainers.length);
         var notFullStoragePlaces = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                     return (structure.structureType == STRUCTURE_STORAGE) &&
