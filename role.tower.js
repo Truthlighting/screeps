68589803bd6b212df096d310b88e436fc725d1b8
truthlighting <truthlighting@gmail.com>
2016-09-19 20:32:23 -0700

    fix towers

--- role.tower.js
+++ role.tower.js
@@ -3,19 +3,23 @@
  */
 var roleTower = {
     run: function() {
-        var towers = Game.getObjectById('TOWER_ID');
-        if (towers) {
-            var closestHostile = towers.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-            var closestDamagedStructure = towers.pos.findClosestByRange(FIND_STRUCTURES, {
-                    filter: (structure) => structure.hits < structure.hitsMax
-            });
+        var towers = [Game.getObjectById('57e09e99e14d5451484ad4b8'), Game.getObjectById('57db653e0f3c649e5b7c3cfe')];
+        if (towers.length > 0) {
+            for (i=0; i < towers.length; i++) {
 
-            if (closestHostile) {
-                towers.attack(closestHostile);
-            }
+                var closestHostile = towers[i].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                var closestDamagedStructure = towers[i].pos.findClosestByRange(FIND_STRUCTURES, {
+                        filter: (structure) = > structure.hits < structure.hitsMax
+            })
+                ;
 
-            if (closestDamagedStructure) {
-                tower.repair(closestDamagedStructure);
+                if (closestHostile) {
+                    towers[i].attack(closestHostile);
+                }
+
+                if (closestDamagedStructure) {
+                    towers[i].repair(closestDamagedStructure);
+                }
             }
         }
     }
