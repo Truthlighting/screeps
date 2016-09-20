3c3afeba06af9f5b94ed82a1a36e82b277fe13ce
truthlighting <truthlighting@gmail.com>
2016-09-20 15:33:45 -0700

    Fixing miner

--- role_prototype.js
+++ role_prototype.js
@@ -69,8 +69,8 @@ var proto = {
 
         for(var i in parts)
         {
-            console.log(i);
-            console.log("parts[i].length (" + parts[i].length + ") - 5 (" + parts[i].length - 5 + ") <= extentions (" + extensions + ")");
+            console.log("i:" + i);
+            console.log("parts[i].length (" + parts[i].length + ") - 5 (" + (parts[i].length - 5) + ") <= extentions (" + extensions + ")");
             if((parts[i].length - 5) <= extensions) {
                 return parts[i];
             }
