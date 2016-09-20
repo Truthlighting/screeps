b21b31340aec07dcc71bd08327ebb10ec931d9e9
truthlighting <truthlighting@gmail.com>
2016-09-19 22:06:56 -0700

    debugging new code for harvesters to assign them to a source

--- main.js
+++ main.js
@@ -33,8 +33,8 @@ module.exports.loop = function () {
 //    console.log('Harvesters: ' + harvesters.length);
     var sources = Game.rooms['W2N48'].find(FIND_SOURCES);
     var harvesterSources = _.map(Game.creeps, 'memory.assignedSource');
-    let result;
-    for (let i=0, l=sources.length; i < l, i++) {
+    var result;
+    for (var i=0, l=sources.length; i < l, i++) {
         if (harvesterSources.indexOf(array[i]) == -1) { result = array[i]; break; }
     }
     console.log(result);
