30e9bc4f7a0c99dcc0a918881451cec187bb77d8
truthlighting <truthlighting@gmail.com>
2016-09-20 15:55:21 -0700

    Fixing miner

--- spawner.js
+++ spawner.js
@@ -107,7 +107,7 @@ module.exports =
             spawnPoint = Game.spawns['Harmony'];
         }
         //console.log(role);
-        console.log("spawnPoint: " + spawnPoint + " spawnPoint.energy: " + spawnPoint.energy + " this.room.energyAvailable: " + this.room.energyAvailable);
+        console.log("spawnPoint: " + spawnPoint + " spawnPoint.energy: " + spawnPoint.energy + " this.room.energyAvailable: " + spawnPoint.room.energyAvailable);
         return spawnPoint.energy >= this.spawnCost(role)
             && (spawnPoint.spawning == null
             || spawnPoint.spawning == undefined);
