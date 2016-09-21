2b8c198d7cb366cfd9cf17e37dda7e3c68754a36
truthlighting <truthlighting@gmail.com>
2016-09-20 17:43:45 -0700

    Fixing miner_helper

--- roles_miner_helper.js
+++ roles_miner_helper.js
@@ -39,7 +39,6 @@ var helper = {
      * @TODO: When spawns are at .25, and extensions have >= 200, help builders before filling shit up
      */
     action: function () {
-        console.log("i'm here");
         var creep = this.creep;
 
         if (creep.memory.courier !== undefined && creep.memory.courier == true) {
