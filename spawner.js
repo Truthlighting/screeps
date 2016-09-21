6c4afa25c77a4e6365d39830827430481d7c8438
truthlighting <truthlighting@gmail.com>
2016-09-20 22:53:08 -0700

    fixing miner

--- spawner.js
+++ spawner.js
@@ -44,7 +44,7 @@ module.exports =
             role = { type: role, memory: { } };
         }
         var me = this;
-        console.log(spawns + " role: " + role + " role.type " + role.type);
+        //console.log(spawns + " role: " + role + " role.type " + role.type);
         var toSpawnAt = spawns.filter(function(spawn)
         {
             //console.log("spawn: " + spawn + " role.type: " + role.type);
