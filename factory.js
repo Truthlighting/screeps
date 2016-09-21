84bde640fed59c84d3dcb3a2044b4250c8aa2ca8
truthlighting <truthlighting@gmail.com>
2016-09-20 20:57:43 -0700

    fixing miner

--- factory.js
+++ factory.js
@@ -8,7 +8,7 @@ module.exports ={
     {
         if(Memory.factoryInit != undefined)
             return;
-
+        console.log("Initializing factory.");
         Memory.factoryInit = true;
         this.memory();
     },
